80b3fbbfa20462229b2b786f6f7943473dc9ff7c
zzh <laofuziCTO@163.com>
2023-11-30 21:32:46 +0800

    修改路金

--- .vuepress/config.js
+++ .vuepress/config.js
@@ -1,7 +1,7 @@
 module.exports = {
   title: "记录",
   description: 'A simple and beautiful vuepress blog theme .',
-  base:'/',
+  base:'/myblog/dist/',
   dest: './dist',
   head: [
     ['link', { rel: 'icon', href: '/favicon.ico' }],
